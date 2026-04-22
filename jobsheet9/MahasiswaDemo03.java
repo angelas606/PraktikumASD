b17052e1f08dd0e0d694b52fd9687839c2f96db6
angelas606 <angelnyrt607@gmail.com>
2026-04-22 22:18:46 +0700

    percobaan2

--- jobsheet9/MahasiswaDemo03.java
+++ jobsheet9/MahasiswaDemo03.java
@@ -41,6 +41,8 @@ public class MahasiswaDemo03 {
                         int nilai = sc.nextInt();
                         dinilai.tugasDinilai(nilai);
                         System.out.printf("Nilai tugas %s adalah %d\n", dinilai.nama, nilai);
+                        String biner = stack.konversiDesimalKeBiner(nilai);
+                        System.out.println("Nilai Biner Tugas: " + biner);
                     }
                     System.out.println();
                     break;
