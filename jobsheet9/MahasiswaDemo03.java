59183776cd2d587f5f8548e57094f0ecf80ddff0
angelas606 <angelnyrt607@gmail.com>
2026-04-22 18:54:48 +0700

    percobaan1

---
+++ jobsheet9/MahasiswaDemo03.java
@@ -0,0 +1,63 @@
+package jobsheet9;
+
+import java.util.Scanner;
+
+public class MahasiswaDemo03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        StackTugasMahasiswa03 stack = new StackTugasMahasiswa03(5);
+        int pilih;
+
+        do {
+            System.out.println("Menu:");
+            System.out.println("1. Mengumpulkan Tugas");
+            System.out.println("2. Menilai Tugas");
+            System.out.println("3. Melihat Tugas Teratas");
+            System.out.println("4. Melihat Daftar Tugas");
+            System.out.print("Pilih: ");
+            pilih = sc.nextInt();
+            sc.nextLine();
+            switch (pilih) {
+                case 1:
+                    System.out.print("Nama: ");
+                    String nama = sc.nextLine();
+                    System.out.print("NIM: ");
+                    String nim = sc.nextLine();
+                    System.out.print("Kelas: ");
+                    String kelas = sc.nextLine();
+                    Mahasiswa03 mhs = new Mahasiswa03(nama, nim, kelas);
+                    stack.push(mhs);
+                    System.out.printf("Tugas %s berhasil dikumpulkan\n", mhs.nama);
+                    System.out.println();
+                    break;
+                case 2:
+                    Mahasiswa03 dinilai = stack.pop();
+                    if (dinilai != null) {
+                        System.out.println("Menilai tugas dari " + dinilai.nama);
+                        System.out.print("Masukkan nilai (0-100): ");
+                        int nilai = sc.nextInt();
+                        dinilai.tugasDinilai(nilai);
+                        System.out.printf("Nilai tugas %s adalah %d\n", dinilai.nama, nilai);
+                    }
+                    System.out.println();
+                    break;
+                case 3:
+                    Mahasiswa03 lihat = stack.peek();
+                    if (lihat != null) {
+                        System.out.println("Tugas terakhir dikumpulkan oleh " + lihat.nama);
+                    }
+                    System.out.println();
+                    break;
+                case 4:
+                    System.out.println("Daftar semua tugas");
+                    System.out.println("Nama\tNIM\tKelas");
+                    stack.print();
+                    System.out.println();
+                    break;
+                default:
+                    System.out.println("Pilihan tidak valid.");
+            }
+        } while (pilih >= 1 && pilih <=4);
+    }
+}
